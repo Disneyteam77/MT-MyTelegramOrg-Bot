2e78675314bedf5b31b27409005fcc9a20222e45
MRK-YT <81806065+MRK-YT@users.noreply.github.com>
2021-05-11 20:05:58 +0530

    👨‍💻Create Bot Menu
    MRK-YT

--- translation.py
+++ translation.py
@@ -1,1 +1,28 @@
+class Translation(object):
+    START_TEXT = """👋𝙷𝚎𝚕𝚕𝚘!
+𝙿𝚕𝚎𝚊𝚜𝚎 𝚁𝚎𝚊𝚍 𝚃𝚑𝚎 𝚃𝚗𝙲 𝙱𝚎𝚏𝚘𝚛𝚎 𝙿𝚛𝚘𝚌𝚎𝚎𝚍𝚒𝚗𝚐: :- <a href='t.me/mo_Tech_yt/38'>𝐂𝐥𝐢𝐜𝐤 𝐇𝐞𝐫𝐞</a>
 
+𝚃𝚑𝚊𝚗𝚔 𝚈𝚘𝚞 𝙵𝚘𝚛 𝚄𝚜𝚒𝚗𝚐 𝙼𝚎 😎
+
+𝙴𝚗𝚝𝚎𝚛 𝚈𝚘𝚞𝚛 𝚃𝚎𝚕𝚎𝚐𝚛𝚊𝚖 𝙿𝚑𝚘𝚗𝚎 𝙽𝚞𝚖𝚋𝚎𝚛, 𝚃𝚘 𝙶𝚎𝚝 𝚃𝚑𝚎 𝙰𝙿𝙿-𝙸𝙳 𝙵𝚛𝚘𝚖 <a href='my.telegram.org/auth'>𝐂𝐥𝐢𝐜𝐤 𝐇𝐞𝐫𝐞</a>
+
+🔊𝙼𝚃 𝙱𝙾𝚃 𝚄𝙿𝙳𝙰𝚃𝙴𝚂 :- @Mo_Tech_YT
+
+/Start 𝙰𝚝 𝙰𝚗𝚢 𝚂𝚝𝚊𝚐𝚎 𝚃𝚘 𝚁𝚎-𝙴𝚗𝚝𝚎𝚛 𝚈𝚘𝚞𝚛 𝙳𝚎𝚝𝚊𝚒𝚕𝚜"""
+    AFTER_RECVD_CODE_TEXT = """𝙸 𝚂𝚎𝚎🙀!
+𝙽𝚘𝚠 🙏𝙿𝚕𝚎𝚊𝚜𝚎 𝚂𝚎𝚗𝚍 𝚃𝚑𝚎 𝚃𝚎𝚕𝚎𝚐𝚛𝚊𝚖 📋𝙲𝚘𝚍𝚎 𝚃𝚑𝚊𝚝 𝚈𝚘𝚞 𝚁𝚎𝚌𝚎𝚒𝚟𝚎𝚍 𝙵𝚛𝚘𝚖 𝚃𝚎𝚕𝚎𝚐𝚛𝚊𝚖!
+
+𝚃𝚑𝚒𝚜 𝙲𝚘𝚍𝚎 𝙸𝚜 𝙾𝚗𝚕𝚢 𝚄𝚜𝚎𝚍 𝙵𝚘𝚛 𝚃𝚑𝚎 𝙿𝚞𝚛𝚙𝚘𝚜𝚎 𝙾𝚏 𝙶𝚎𝚝𝚝𝚒𝚗𝚐 𝚃𝚑𝚎 𝙰𝙿𝙿 𝙸𝙳 𝙵𝚛𝚘𝚖 <a href='my.telegram.org/auth'>𝐂𝐥𝐢𝐜𝐤 𝐇𝐞𝐫𝐞</a>
+
+𝙸𝚏 𝚈𝚘𝚞 𝙳𝚘 𝙽𝚘𝚝 𝚃𝚛𝚞𝚜𝚝 𝚃𝚑𝚒𝚜 𝙱𝚘𝚝 𝙳𝚎𝚟, 🙏𝙿𝚕𝚎𝚊𝚜𝚎 𝙷𝚘𝚜𝚝 𝚃𝚑𝚒𝚜 🤖𝙱𝚘𝚝 𝚈𝚘𝚞𝚛𝚂𝚎𝚕𝚏
+
+𝐍𝐨𝐭𝐢𝐜𝐞
+𝙿𝚕𝚎𝚊𝚜𝚎 𝚁𝚎𝚊𝚍 𝚃𝚑𝚎 𝚃𝚗𝙲 𝙱𝚎𝚏𝚘𝚛𝚎 𝙿𝚛𝚘𝚌𝚎𝚎𝚍𝚒𝚗𝚐 <a href='https://t.me/Mo_Tech_YT/38'>𝐑𝐞𝐚𝐝 𝐌𝐞</a>
+🔊𝙼𝚃 𝙱𝙾𝚃 𝚄𝙿𝙳𝙰𝚃𝙴𝚂 :- @Mo_Tech_YT
+
+/Start 𝙰𝚝 𝙰𝚗𝚢 𝚂𝚝𝚊𝚐𝚎 𝚃𝚘 𝚁𝚎-𝙴𝚗𝚝𝚎𝚛 𝚈𝚘𝚞𝚛 𝙳𝚎𝚝𝚊𝚒𝚕𝚜"""
+    BEFORE_SUCC_LOGIN = "recieved code. Scarpping web page ..."
+    ERRED_PAGE = "something wrongings. failed to get app id. \n\n@Mo_Tech_YT"
+    CANCELLED_MESG = "Bye! Please re /start the bot conversation"
+    IN_VALID_CODE_PVDED = "sorry, but the input does not seem to be a valid Telegram Web-Login code"
+    IN_VALID_PHNO_PVDED = "sorry, but the input does not seem to be a valid phone number"
