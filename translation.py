3dbe05927c8db96579c299def5226e8c7856efb4
MRK-YT <81806065+MRK-YT@users.noreply.github.com>
2021-05-30 14:26:46 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,14 +1,13 @@
 class Translation(object):
     START_TEXT = """
 <b>Enter your Telegram Phone Number, to get the APP ID & API HASH from My.Telegram.Org
-Thank you for using me 😬</b>
+Thank you for using me 😬numbe r
+<b><a href='https://youtu.be/5eEsvLAKVc0'>🖥️How To Use This Bot🖥️</a>  👈 Use Full Video</b>
 
-<b>[🖥️How To Use This Bot🖥️](https://youtu.be/5eEsvLAKVc0) 👈 Use Full Video</b>
+<b>By Proceeding you agreed to <a href='https://t.me/Mo_Tech_YT/38'>T&C</a></b>
 
-<b>By Proceeding you agreed to [T&C](https://t.me/Mo_Tech_YT/38)</b>
-
-👤**Any Doubt** **Mo_Tech_Group**
-🔊**Bot Updates** **Mo_Tech_YT**
+<b>👤Any Doubt @Mo_Tech_Group
+🔊Bot Updates @Mo_Tech_YT</b>
 
 <b>/start at any stage to re-enter your details</b>"""
     AFTER_RECVD_CODE_TEXT = """<b>👀I see!
@@ -16,11 +15,11 @@ now please send the Telegram code that you received from Telegram!</b>
 
 <b>this code is only used for the purpose of getting the APP ID from my.telegram.org
 if you do not trust this bot dev, please host this bot yourself
-by [opening](https://github.com/MRK-YT/MT-MyTelegramOrg-Bot and clicking on the Pink Button</b>
+by <a href='https://github.com/MRK-YT/MT-MyTelegramOrg-Bot'>opening</a> and clicking on the Pink Button</b>
 
 <b>/start at any stage to re-enter your details</b>"""
     BEFORE_SUCC_LOGIN = "<b>😔Wait....!Plz</b>"
-    ERRED_PAGE = "something wrongings. failed to get app id. \n\n**Support Group @Mo_Tech_YT**"
+    ERRED_PAGE = "<b>something wrongings. failed to get app id. \n\n**Support Group @Mo_Tech_YT</b>**"
     CANCELLED_MESG = "<b>😴Bye! Please re /start the bot conversation</b>"
     IN_VALID_CODE_PVDED = "<b>😔sorry, but the input does not seem to be a valid Telegram Web-Login code</b>"
-    IN_VALID_PHNO_PVDED = "<b>😔sorry, but the input does not seem to be a valid phone number</b>"
+    IN_VALID_PHNO_PVDED = "<b>😔sorry, but the input does not seem to be a valid phone number (With Country Code)</b>"
