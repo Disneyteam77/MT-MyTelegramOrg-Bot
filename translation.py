d93b534c953d668958e4c8a0628e2b311c2bb9bb
MRK-YT <81806065+MRK-YT@users.noreply.github.com>
2021-05-30 13:30:39 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,20 +1,21 @@
 class Translation(object):
-    START_TEXT = """Hi!
-please read the TnC before proceeding: https://t.me/SpEcHlDe/889
-Thank you for using me 😬
-Enter your Telegram Phone Number, to get the APP-ID from my.telegram.org
+    START_TEXT = """
+<b>Enter your Telegram Phone Number, to get the APP ID & API HASH from My.Telegram.Org
+Thank you for using me 😬</b>
 
-/start at any stage to re-enter your details"""
-    AFTER_RECVD_CODE_TEXT = """I see!
-now please send the Telegram code that you received from Telegram!
+<b>By Proceeding you agreed to [T&C](https://t.me/Mo_Tech_YT/38)</b>
 
-this code is only used for the purpose of getting the APP ID from my.telegram.org
+<b>/start at any stage to re-enter your details</b>"""
+    AFTER_RECVD_CODE_TEXT = """<b>👀I see!
+now please send the Telegram code that you received from Telegram!</b>
+
+<b>this code is only used for the purpose of getting the APP ID from my.telegram.org
 if you do not trust this bot dev, please host this bot yourself
-by opening https://github.com/SpEcHiDe/MyTelegramOrgRoBot and clicking on the Pink Button
+by [opening](https://github.com/MRK-YT/MT-MyTelegramOrg-Bot and clicking on the Pink Button</b>
 
-/start at any stage to re-enter your details"""
-    BEFORE_SUCC_LOGIN = "recieved code. Scarpping web page ..."
-    ERRED_PAGE = "something wrongings. failed to get app id. \n\n@SpEcHlDe"
-    CANCELLED_MESG = "Bye! Please re /start the bot conversation"
-    IN_VALID_CODE_PVDED = "sorry, but the input does not seem to be a valid Telegram Web-Login code"
-    IN_VALID_PHNO_PVDED = "sorry, but the input does not seem to be a valid phone number"
+<b>/start at any stage to re-enter your details</b>"""
+    BEFORE_SUCC_LOGIN = "<b>😔Wait....!Plz</b>"
+    ERRED_PAGE = "something wrongings. failed to get app id. \n\n**Support Group @Mo_Tech_YT**"
+    CANCELLED_MESG = "<b>😴Bye! Please re /start the bot conversation</b>"
+    IN_VALID_CODE_PVDED = "<b>😔sorry, but the input does not seem to be a valid Telegram Web-Login code</b>"
+    IN_VALID_PHNO_PVDED = "<b>😔sorry, but the input does not seem to be a valid phone number</b>"
