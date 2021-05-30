39973be4c37ec048512f20507271981a06fc9ea8
MRK-YT <81806065+MRK-YT@users.noreply.github.com>
2021-05-30 14:58:11 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,26 +1,17 @@
 class Translation(object):
-    START_TEXT = """<b>Hi!</b>
-<b>Enter your Telegram Phone Number, to get the APP ID & API HASH from My.Telegram.Org
-Thank you for using me 😬number
-
-<b>🖥️How To Use This Bot https://youtu.be/5eEsvLAKVc0 👈 Use Full Video</b>
-
-<b>By Proceeding you agreed to</b> [T&C](https://t.me/Mo_Tech_YT/38)
-
-<b>👤Any Doubt @Mo_Tech_Group
-🔊Bot Updates @Mo_Tech_YT</b>
-
-<b>/start at any stage to re-enter your details</b>"""
-    AFTER_RECVD_CODE_TEXT = """<b>👀I see!
-now please send the Telegram code that you received from Telegram!</b>
-
-<b>this code is only used for the purpose of getting the APP ID from my.telegram.org
+    START_TEXT = """Hi!
+<b>Enter your Telegram Phone Number, to get the APP-ID from my.telegram.org</b>\n
+\n<b>please read the TnC before proceeding: https://t.me/Mo_Tech_YT/38\n
+\n<b>Thank you for using me 😬</b>\n\n🖥️How To Use This Bot🖥️👉https://youtu.be/5eEsvLAKVc0\n\n👤Any Doubt @Mo_Tech_Group\n🤖Bot Update @Mo_Tech_YT</b>\n
+/start at any stage to re-enter your details"""
+    AFTER_RECVD_CODE_TEXT = """<b>I see!
+now please send the Telegram code that you received from Telegram!
+this code is only used for the purpose of getting the APP ID from my.telegram.org
 if you do not trust this bot dev, please host this bot yourself
-by opening https://github.com/MRK-YT/MT-MyTelegramOrg-Bot and clicking on the Pink Button</b>
-
-<b>/start at any stage to re-enter your details</b>"""
-    BEFORE_SUCC_LOGIN = "<b>😔Wait....!Plz</b>"
+by opening https://github.com/MRK-Y/MT-MyTelegramOrg-Bot and clicking on the Pink Button</b
+/start at any stage to re-enter your details"""
+    BEFORE_SUCC_LOGIN = "recieved code. Scarpping web page ..."
     ERRED_PAGE = "<b>something wrongings. failed to get app id. \n\nSupport Group @Mo_Tech_YT</b>"
-    CANCELLED_MESG = "<b>😴Bye! Please re /start the bot conversation</b>"
-    IN_VALID_CODE_PVDED = "<b>😔sorry, but the input does not seem to be a valid Telegram Web-Login code</b>"
-    IN_VALID_PHNO_PVDED = "<b>😔sorry, but the input does not seem to be a valid phone number [With Country Code]</b>"
+    CANCELLED_MESG = "Bye! Please re /start the bot conversation"
+    IN_VALID_CODE_PVDED = "sorry, but the input does not seem to be a valid Telegram Web-Login code"
+    IN_VALID_PHNO_PVDED = "sorry, but the input does not seem to be a valid phone number"
