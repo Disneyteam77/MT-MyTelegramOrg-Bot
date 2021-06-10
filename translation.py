c95ae90d23296a50f047dbb004e438621ec43fc6
MRK-YT <81806065+MRK-YT@users.noreply.github.com>
2021-06-10 19:18:41 +0530

    Update

--- translation.py
+++ translation.py
@@ -1,6 +1,6 @@
 class Translation(object):
     START_TEXT = """Hi!
-🖥️How To Use This Bot🖥️ https://youtu.be/5eEsvLAKVc0 \n\nplease read the TnC before proceeding: https://t.me/SpEcHlDe/889
+🖥️How To Use This Bot🖥️ https://youtu.be/5eEsvLAKVc0 \n\nplease read the TnC before proceeding: https://t.me/Mo_Tech_YT/122
 \n👤Any Doubt @Mo_Tech_Group\n🤖Bot Update @Mo_Tech_YT\n
 Enter your Telegram Phone Number, to get the APP-ID from my.telegram.org
 
@@ -14,7 +14,7 @@ by opening https://github.com/MRK-YT/MT-MyTelegramOrg-Bot and clicking on the Pi
 
 /start at any stage to re-enter your details"""
     BEFORE_SUCC_LOGIN = "recieved code. Scarpping web page ..."
-    ERRED_PAGE = "something wrongings. failed to get app id. \n\n@Mo_Tech_Group"
+    ERRED_PAGE = "something wrongings. failed to get app id. \n\n@Mo_Tech_Group\n\nHow Get Api Code For Website https://youtu.be/5eEsvLAKVc0"
     CANCELLED_MESG = "Bye! Please re /start the bot conversation"
     IN_VALID_CODE_PVDED = "sorry, but the input does not seem to be a valid Telegram Web-Login code"
     IN_VALID_PHNO_PVDED = "sorry, but the input does not seem to be a valid phone number"
