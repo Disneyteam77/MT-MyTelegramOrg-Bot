32026fd0d5073dc325e787032d85330497adea31
Disneyteam <85325455+Disneyteam76@users.noreply.github.com>
2021-06-29 08:36:58 +0530

    Update

--- translation.py
+++ translation.py
@@ -1,7 +1,7 @@
 class Translation(object):
     START_TEXT = """Hi!
-🖥️How To Use This Bot🖥️ https://youtu.be/5eEsvLAKVc0 \n\nplease read the TnC before proceeding: https://t.me/Mo_Tech_YT/122
-\n👤Any Doubt @Mo_Tech_Group\n🤖Bot Update @Mo_Tech_YT\n
+I am Disney Team My.telegram.org BOT BY @doreamonfans1 
+\n👤Any Doubt @disneyteamchat\n🤖Bot Update @disneygrou\n
 Enter your Telegram Phone Number, to get the APP-ID from my.telegram.org
 
 /start at any stage to re-enter your details"""
@@ -9,12 +9,11 @@ Enter your Telegram Phone Number, to get the APP-ID from my.telegram.org
 now please send the Telegram code that you received from Telegram!
 
 this code is only used for the purpose of getting the APP ID from my.telegram.org
-if you do not trust this bot dev, please host this bot yourself
-by opening https://github.com/MRK-YT/MT-MyTelegramOrg-Bot and clicking on the Pink Button
+By @doreamonfans1 
 
 /start at any stage to re-enter your details"""
     BEFORE_SUCC_LOGIN = "recieved code. Scarpping web page ..."
-    ERRED_PAGE = "something wrongings. failed to get app id. \n\n@Mo_Tech_Group\n\nHow Get Api Code For Website https://youtu.be/5eEsvLAKVc0"
+    ERRED_PAGE = "something wrongings. failed to get app id. \n\n@disneyteamchat"
     CANCELLED_MESG = "Bye! Please re /start the bot conversation"
     IN_VALID_CODE_PVDED = "sorry, but the input does not seem to be a valid Telegram Web-Login code"
     IN_VALID_PHNO_PVDED = "sorry, but the input does not seem to be a valid phone number"
